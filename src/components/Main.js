978509f6734b271c8bf527f06ec66dfcbceec44d
Aryan Gupta <aryanguptabareilly067@gmail.com>
2023-09-28 01:40:07 +0530

    commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,12 +5,28 @@ const Main = () => {
         <div id='mainContainer'>
             <div id='main'>
                 <div id="chatList">
-                    <div id="statusBar" className='flex'>
+                    <div id="topNav" className='flex'>
+
                         <div id="profilePhoto">
                             <img src={require("./profilePhoto.png")} alt="" />
                         </div>
-                        <div id="status">
-                            <img src={require("./status.png")} alt="" />
+
+                        <div className='flex' id='statusSection'>
+                            <div id="communities">
+                                <img src={require("./communities.png")} alt="" />
+                            </div>
+                            <div id="status">
+                                <img src={require("./status.png")} alt="" />
+                            </div>
+                            <div id="channels">
+                                <img src={require("./channels.png")} alt="" />
+                            </div>
+                            <div id="newChat">
+                                <img src={require("./newChat.png")} alt="" />
+                            </div>
+                            <div id="menu">
+                                <img src={require("./menu.png")} alt="" />
+                            </div>
                         </div>
                     </div>
                     <div id="searchBar">
