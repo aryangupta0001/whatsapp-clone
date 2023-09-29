77130dff76d6d1edc66131a71382011653042b3d
Aryan Gupta <aryanguptabareilly067@gmail.com>
2023-09-30 01:34:51 +0530

    message area layout made

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,41 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Main = () => {
 
     const [chatSearch, setChatSearch] = useState("");
-    
-    const onChange = (e) => {
-        try {
-            setChatSearch(e.target.value);
-        }
-        catch { }
 
+    useEffect(() => {
         if (chatSearch.length === 0) {
             document.getElementById("clearChatSearch").style.display = "none";
-        }
-        else {
+        } else {
             document.getElementById("clearChatSearch").style.display = "block";
         }
 
         console.log(chatSearch.length);
+    }, [chatSearch]);
+
+    const onChange = async (e) => {
+        setChatSearch(e.target.value);
+    }
+
+    const chatList = {
+        "Abhisek Tamang": "E:/RANBEER/CODE/WEB DEV/MERN/REACT/whatsapp-clone/src/components/chats/Abhisek Tamang.jpg",
+        "ANDREW LINDER": "E:/RANBEER/CODE/WEB DEV/MERN/REACT/whatsapp-clone/src/components/chats/ANDREW LINDER.jpg",
+        "Jonah Hill": "E:/RANBEER/CODE/WEB DEV/MERN/REACT/whatsapp-clone/src/components/chats/Jonah Hill.webp",
+        "Chris Evans": "E:/RANBEER/CODE/WEB DEV/MERN/REACT/whatsapp-clone/src/components/chats/Chris Evans.jpg",
+        "Sandeep Nainwal": "E:/RANBEER/CODE/WEB DEV/MERN/REACT/whatsapp-clone/src/components/chats/Sandeep Nainwal.jpg",
+        "Michael": "E:/RANBEER/CODE/WEB DEV/MERN/REACT/whatsapp-clone/src/components/chats/Michael.jpg",
+        "Johnny Depp": "E:/RANBEER/CODE/WEB DEV/MERN/REACT/whatsapp-clone/src/components/chats/Johnny Depp.webp",
+        "Hani Abdel-Warith": "E:/RANBEER/CODE/WEB DEV/MERN/REACT/whatsapp-clone/src/components/chats/Hani Abdel-Warith.jpeg",
+        "Chris Pratt": "E:/RANBEER/CODE/WEB DEV/MERN/REACT/whatsapp-clone/src/components/chats/Chris Pratt.avif",
+        "Ramit Sethi": "E:/RANBEER/CODE/WEB DEV/MERN/REACT/whatsapp-clone/src/components/chats/Ramit Sethi.jpg",
+        "Rocco Giannetti": "E:/RANBEER/CODE/WEB DEV/MERN/REACT/whatsapp-clone/src/components/chats/Rocco Giannetti.jpg",
+        "Will Smith": "E:/RANBEER/CODE/WEB DEV/MERN/REACT/whatsapp-clone/src/components/chats/Will Smith.jpg"
     }
 
     return (
         <div id='mainContainer'>
-            <div id='main'>
+            <div id='main' className='flex'>
                 <div id="chatList">
                     <div id="topNav" className='flex'>
 
@@ -31,21 +44,11 @@ const Main = () => {
                         </div>
 
                         <div className='flex' id='statusSection'>
-                            {/* <div id="communities"> */}
                             <img src={require("./communities.png")} alt="" />
-                            {/* </div> */}
-                            {/* <div id="status"> */}
                             <img src={require("./status.png")} alt="" />
-                            {/* </div> */}
-                            {/* <div id="channels"> */}
                             <img src={require("./channels.png")} alt="" />
-                            {/* </div> */}
-                            {/* <div id="newChat"> */}
                             <img src={require("./newChat.png")} alt="" />
-                            {/* </div> */}
-                            {/* <div id="menu"> */}
                             <img src={require("./menu.png")} alt="" />
-                            {/* </div> */}
                         </div>
                     </div>
 
@@ -59,16 +62,41 @@ const Main = () => {
 
                     </div>
 
+                    <div id="archived" className='flex'>
+                        <div className='flex'>
+                            <img src={require("./archived.png")} alt="" />
+                        </div>
+
+                        <div className='flex'>
+                            Archived
+                        </div>
+                    </div>
+
                     <div id="chats">
 
                     </div>
                 </div>
 
-                <div id="messages">
+                <div id="messages" className='flex'>
+                    <div className='flex'>
+                        <img src={require("./whatsapp.png")} alt="" />
+                        <h1>
+                            Download WhatsApp for Windows
+                        </h1>
 
+                        <p>
+                            Make calls, share your screen and get a faster experience when you download the Windows app.
+                        </p>
+
+                        <div id="getAppBtn">
+                            Get the app
+                        </div>
+                    </div>
+
+                    <p>Your personal messages are end-to-end encrypted</p>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
